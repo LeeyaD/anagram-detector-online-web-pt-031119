52edbd31b795b2978f06498c901293c71d9acba3
Leeya Davis <leeya463@gmail.com>
2019-04-06 23:40:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+#given a word & array of possible anagrams, selects the correct ones
+#class instances should respond to #match, which takes an array of possible anagrams
+#all matches should be returned in an array
+#if no matches exist, an empty array should be returned
+require 'pry'
+class Anagram
+  attr_accessor :word
+#create a class Anagram with an #initialize and use attr_accessor (for word that gets initialized)
+  def initialize (word)
+    @word = word
+  end
+
+  def match (matches)
+
+  end
+
+end
